09f5427397dfd09dc6dc9e0db85785faa6a015b8
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-03-24 07:23:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ name="a",
 bot_token=bot_token
 ,api_id=api_id
 ,api_hash=api_hash,
-plugins={"root":"Plugins","include":["start","channel_force","callbackquery","extra","admin","main"]})
+plugins={"root":"Plugins","include":["start","channel_force","callbackquery","extra","admin","ai"]})
 
 main_bot.run()
