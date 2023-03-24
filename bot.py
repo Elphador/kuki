9e4c67b79bd22c2b1a82f9c52900828559b157bb
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-03-24 07:26:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ from pyrogram import Client as app
 from config import *
 
 main_bot = app(
-name="a",
+name="ak",
 bot_token=bot_token
 ,api_id=api_id
 ,api_hash=api_hash,
-plugins={"root":"Plugins","include":["start","channel_force","callbackquery","extra","admin","ai"]})
+plugins={"root":"Plugins","include":["start","channel_force","callbackquery","extra","ai","admin"]})
 
 main_bot.run()
