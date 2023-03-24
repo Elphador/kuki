375750554ed4f30fe8b5b920cb8e43aed3790800
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-03-24 07:03:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 from pyrogram import Client as app
 from config import *
-proxy = {
-     "scheme":"socks5",
-     "hostname": "146.59.152.52",
-     "port": 59166,
-}
+
 main_bot = app(
 name="a",
-proxy=proxy,
-ipv6=True,
 bot_token=bot_token
 ,api_id=api_id
 ,api_hash=api_hash,
