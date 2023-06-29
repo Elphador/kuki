9b9baf4031158797192e95dfccc0073ff0c3e423
shamil <im.shamxl@gmail.com>
2023-06-29 21:33:06 +0530

    Update bot.py
    If you specify the folder name you do not need to specify the name of each file

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ name="ak",
 bot_token=bot_token
 ,api_id=api_id
 ,api_hash=api_hash,
-plugins={"root":"Plugins","include":["start","channel_force","callbackquery","extra","ai","admin"]})
+plugins={"root":"Plugins"})
 
 main_bot.run()
